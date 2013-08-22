86ed3d540fda25939d53b81d102bf98d9bf8467a
bloodynumen <bloodynumen@163.com>
2013-08-22 17:59:06 +0800

    add plugin qingyang

---
+++ qingyangticket/src/ticket.js
@@ -0,0 +1,11 @@
+$().ready(function(){
+    url = 'http://music.baidu.com/huodong/vote/vote?code=qingyang&id=1&_=1377159604926';
+    //$.ajax({
+    //    url : url,
+    //    success: function(){
+    //        alert(1);
+    //    }
+    //});
+    //setInterval(function(){
+    //}, 500);
+});
