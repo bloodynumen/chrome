8b269d9bd4f290b3d82a1d3ac2920edd9ed230c1
bloodynumen <bloodynumen@163.com>
2013-08-23 13:26:09 +0800

    delete so]

--- ad/src/ad.js
+++
@@ -1,7 +0,0 @@
-$().ready(function(){
-    $('.ad-index-left,.ecom-banner').remove()
-    setInterval(function(){
-        $('.ecom-ads-wrap').remove();
-        $('.ui-reelList-viewport').css('bottom','0');
-    }, 500);
-});
